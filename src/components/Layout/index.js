8480e5203e195ebbf222b8b0de406fbe9df34326
Haythem <sinnner6@gmail.com>
2023-01-20 22:23:30 +0100

    implemented sidebar & updated assets

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
+import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
-  return <div>Hello</div>
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
 }
 
 export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,94 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faLinkedin,
+  faSkype,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      {/* <img src={LogoSubtitle} alt="haythem" className="sub-logo" /> */}
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4d" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4d" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4d" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/haythem-ben-drissia/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/3quanimity"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.youtube.com/channel/UCoBKCo-JqN4VKvARojqzP1A"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faYoutube}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a href="skype:live:myUserNameHere" rel="noreferrer" target="_blank">
+          <FontAwesomeIcon
+            icon={faSkype}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
