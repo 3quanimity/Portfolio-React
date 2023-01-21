0946f01295d7808896e942e7ac85723574a6ed56
Haythem <sinnner6@gmail.com>
2023-01-21 17:27:43 +0100

    implemented home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br />
+          I'm Haythem
+          {/* <img src={LogoTitle} alt="developer" /> */}
+          <br />
+          web developer
+        </h1>
+        {/* <h2>Frontend Developer / Gamer / Astronaut</h2> */}
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
