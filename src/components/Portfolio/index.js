938b4bc4d45411d148d5a66000b4f6e6f6a1681b
Haythem <sinnner6@gmail.com>
2023-01-23 23:11:06 +0100

    implemented portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -28,6 +28,14 @@ const Portfolio = () => {
                 src={port.cover}
                 alt={port.title + index}
               />
+
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
             </div>
           )
         })}
